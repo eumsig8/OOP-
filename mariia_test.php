77e6a1fd59d7203135fb2d929b25ce1db43b2f67
Mariia <mariia.kulyk@sa.stud.vu.lt>
2025-03-30 19:59:34 +0300

    u i a i

--- mariia_test.php
+++ mariia_test.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Collatz Conjecture</title>
+    <title>Collatz_Conjecture</title>
 </head>
 <body>
     <h1>Collatz Conjecture</h1>
